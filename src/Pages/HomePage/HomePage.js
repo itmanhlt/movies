e55d11560742466bd3ed0ade579b5aaa10f0221f
manh le <it.manhlt@gmail.com>
2023-04-07 22:54:15 +0700

    update

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -1,12 +1,8 @@
-import React from 'react'
-import Carousels from '../../Components/Carousel/Carousels'
-import { useMediaQuery } from 'react-responsive'
+import React from "react";
+import Carousels from "../../Components/Carousel/Carousels";
+import { useMediaQuery } from "react-responsive";
 
 export default function HomePage() {
-    const isMobile = useMediaQuery({ maxWidth: 767 })
-  return (
-    <div>
-      {isMobile?<></>:<Carousels/>}
-    </div>
-  )
+  const isMobile = useMediaQuery({ maxWidth: 767 });
+  return <div>{isMobile ? <></> : <Carousels />}</div>;
 }
