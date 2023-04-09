b1c55296896ca54a2c0f43268937a801ef1fa758
Giao Quỳnh <giaovan99@gmail.com>
2023-04-09 23:16:20 +0700

    buoi 4 09/04/2023 - bug trailer button (ItemMovie)

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Carousels from "../../Components/Carousel/Carousels";
 import { useMediaQuery } from "react-responsive";
 import SearchMovieMobileTablet from "../../Components/SearchMovie/SearchMovieMobileTablet";
@@ -6,13 +6,32 @@ import SearchMovieDesktop from "../../Components/SearchMovie/SearchMovieDesktop"
 import ListMovie from "../../Components/ListMovie/ListMovie";
 
 export default function HomePage() {
+  let [video, setVideo] = useState("hidden");
+  let [videoURL, setVideoURL] = useState();
+  let handleVideo = (url) => {
+    setVideo(video == "hidden" ? "visible" : "hidden");
+    setVideoURL(url);
+  };
+
   const isMobile = useMediaQuery({ maxWidth: 767 });
-  const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 991 });
+  const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1023 });
   return (
     <div>
-      {isMobile ? <></> : <Carousels />}
-      {isMobile || isTablet ? <SearchMovieMobileTablet/> : <SearchMovieDesktop />}
-      <ListMovie/>
+      {isMobile ? (
+        <></>
+      ) : (
+        <Carousels
+          video={video}
+          handleVideo={handleVideo}
+          videoURL={videoURL}
+        />
+      )}
+      {isMobile || isTablet ? (
+        <SearchMovieMobileTablet />
+      ) : (
+        <SearchMovieDesktop />
+      )}
+      <ListMovie video={video} handleVideo={handleVideo} videoURL={videoURL} />
     </div>
   );
 }
