f18fcb222c93d3aa16aea77d22d5bd7680bb9a09
Giao Quỳnh <giaovan99@gmail.com>
2023-04-15 04:37:24 +0700

    buoi 8 14/04/2023 news

--- src/service/movieService.js
+++ src/service/movieService.js
@@ -5,12 +5,14 @@ export const movieServ = {
     return https.get("/api/QuanLyPhim/LayDanhSachPhim?maNhom=GP09");
   },
   getMovieTheater: () => {
-    return https.get("/api/QuanLyRap/LayThongTinLichChieuHeThongRap?maNhom=GP09");
+    return https.get(
+      "/api/QuanLyRap/LayThongTinLichChieuHeThongRap?maNhom=GP09"
+    );
   },
   getDetailMovie: (id) => {
     return https.get(`/api/QuanLyPhim/LayThongTinPhim?MaPhim=${id}`);
   },
   getSearchMovie: (id) => {
     return https.get(`/api/QuanLyRap/LayThongTinLichChieuPhim?MaPhim=${id}`);
-  }
-}
+  },
+};
